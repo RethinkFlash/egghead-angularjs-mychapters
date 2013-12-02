7c37250f012d4607daf97cb12b1985693c26ee4c
Ken Malley <ken@rethinkcanada.com>
2013-12-02 11:21:04 -0800

    initial commit and starting point for angular tutorials from egghead.io

---
+++ js/app.js
@@ -0,0 +1,15 @@
+/**
+ * Created by kennethmalley on 12/2/13.
+ */
+
+/** remember to add documentation to your code!
+ *  visit https://code.google.com/p/jsdoc-toolkit/w/list
+ *  for how to use @params, @return, etc.
+ *  Also remember to create a .jshintrc file in the root directory
+ */
+
+(function () {
+	"use strict";
+	// this function is strict...
+
+}());
