32da2aec9f1618cc03c263070ce97ae7c87c0a5b
Ken Malley <ken@rethinkcanada.com>
2013-12-06 12:06:46 -0800

    "templateUrl", organize long html templates into their own seperate template file.

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ app.directive("zippy", function() {
 			content: "&"
 		},
 		transclude: true,
-		template:'<div>\n\t<h3 ng-click="toggleContent()">{{ title }}</h3>\n\t<div ng-transclude ng-show="isContentVisible">Hello World</div>\n</div>',
+		templateUrl:'zippy.html',
 		link: function (scope) {
 			scope.isContentVisible = false;
 			scope.toggleContent = function() {
